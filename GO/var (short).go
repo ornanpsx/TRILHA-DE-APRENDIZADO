87c7ac5dfc29de4b0007895b7db6a4102bb59c3f
Ornan <ornanpsx@gmail.com>
2023-02-04 10:24:58 -0300

    create: new files

---
+++ GO/var (short).go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nome := "Ornan Matos"
+	idade := 26
+	var versao float32 = 1.2
+	fmt.Println("Olá", nome, "sua idade é", idade)
+	fmt.Println("A versão do programa é", versao)
+}
---
+++ GO/var.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Ornan"
+	var versao float32 = 1.2
+	var idade int = 26
+	fmt.Println("Olá", nome, "bem-vindo ao nosso terminal")
+	fmt.Println("Verificando a sua idade em nossa Database.")
+	fmt.Println("...")
+	fmt.Println("A sua idade é", idade, "anos.")
+	fmt.Println("Versão:", versao)
+
+}
