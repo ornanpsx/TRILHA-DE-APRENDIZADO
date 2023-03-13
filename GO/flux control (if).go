f237d89c3d30cd0f04b211b353516531de9baf87
Ornan <ornanpsx@gmail.com>
2023-03-13 17:00:17 -0300

    create: control_flux.go

---
+++ GO/flux control (if).go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	nome :=
+	var versao float32 = 1.3
+	var comando int8 =
+	
+	fmt.Println("Digite o seu nome:")
+	fmt.Scan(&nome)
+	fmt.Println("Olá,", nome)
+	fmt.Println("A versão do programa é", versao)
+	fmt.Println("----------------------")
+	fmt.Println("Escolha uma das opções do menu.")
+	fmt.Println("1 - Iniciar Aplicação")
+	fmt.Println("2 - Executar Comando Especifico")
+	fmt.Println("")
+
+
+}
