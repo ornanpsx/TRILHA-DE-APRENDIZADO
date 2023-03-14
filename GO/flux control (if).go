903e47836874416f9396683fb3dd2ca6820fa443
ornanpsx <ornanpsx@gmail.com>
2023-03-13 21:12:36 -0300

    update: fix flux_control(if)

--- GO/flux control (if).go
+++ GO/flux control (if).go
@@ -4,10 +4,9 @@ import "fmt"
 
 func main(){
 
-	nome :=
+	var nome string
 	var versao float32 = 1.3
-	var comando int8 =
-	
+	var comando int8
 	fmt.Println("Digite o seu nome:")
 	fmt.Scan(&nome)
 	fmt.Println("Olá,", nome)
@@ -16,7 +15,20 @@ func main(){
 	fmt.Println("Escolha uma das opções do menu.")
 	fmt.Println("1 - Iniciar Aplicação")
 	fmt.Println("2 - Executar Comando Especifico")
-	fmt.Println("")
+	fmt.Println("3 - Sair")
+	fmt.Scan(&comando)
 
-
-}
+	if comando == 1 {
+		fmt.Println("Iniciando aplicação. Aguarde...")
+		
+	} else if comando == 2 {
+		fmt.Println("Executando comando.")
+		
+	} else if comando == 3 {
+		fmt.Println("Saindo da aplicação.")
+		
+	} else {
+			fmt.Println("Desculpe esse comando não é reconhecido. Tente novamente.")
+	}
+	
+}	
